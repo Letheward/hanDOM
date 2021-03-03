21100272e5a0367d2c79a704b32f5503e2375e3e
Letheward <letheward@foxmail.com>
2021-03-03 23:00:30 +0800

    adding more functions

--- src/main.js
+++ src/main.js
@@ -1,95 +1,124 @@
-let content = document.querySelector("p");
-let leng;
+let lengPara;
+let lengSen;
+
 let punc;
+let period;
 let comma;
+let semicolon;
+let question;
+let exclamation;
+
 let start;
 let end;
 
+function checkInput() {
+    lengPara    = parseInt(document.getElementById("lengPara").value);
+    lengSen     = parseInt(document.getElementById("lengSen").value);
+    punc        = parseFloat(document.getElementById("punc").value);
+    period      = parseFloat(document.getElementById("period").value);
+    comma       = parseFloat(document.getElementById("comma").value);
+    semicolon   = parseFloat(document.getElementById("semicolon").value);
+    question    = parseFloat(document.getElementById("question").value);
+    exclamation = parseFloat(document.getElementById("exclamation").value);
+    start       = parseInt(document.getElementById("start").value);
+    end         = parseInt(document.getElementById("end").value);
+    if (isNaN(lengPara)) lengPara = 500;
+    if (isNaN(lengSen)) lengSen = 6;
+    if (isNaN(punc)) punc = 0.2;
+    if (isNaN(period)) period = 0.3;
+    if (isNaN(comma)) comma = 0.5;
+    if (isNaN(semicolon)) semicolon = 0.1;
+    if (isNaN(question)) question = 0.1;
+    if (isNaN(exclamation)) exclamation = 0.1;
+    if (isNaN(start)) start = 0;
+    if (isNaN(end)) end = 3000;
+}
+
 // let totalChar = 258851464;
 
-function createString(x, punc, comma, start, end) {
+function createString() {
+
     let newString = "";
     let totalChar = 0;
+
     for (i = start; i < end; i++) {
         totalChar += hanArray[i][1];
     }
-    for (i = 0; i < x; i++) {
+
+    for (i = 0; i < lengPara; i++) {
         for (j = start; j < end; j++) {
             if (Math.random() < hanArray[j][1] / totalChar) {
                 newString += hanArray[j][0];
             }
         }
         if (
-            newString.length > 4
+            newString.length > lengSen
             &&
-            newString[newString.length - 1] !== "，"
+            !newString.slice(-lengSen).includes("。")
             &&
-            newString[newString.length - 1] !== "。"
+            !newString.slice(-lengSen).includes("，")
             &&
-            newString[newString.length - 2] !== "，"
+            !newString.slice(-lengSen).includes("；")
             &&
-            newString[newString.length - 2] !== "。"
+            !newString.slice(-lengSen).includes("？")
+            &&
+            !newString.slice(-lengSen).includes("！")
             &&
             Math.random() < punc
+            &&
+            !(lengPara - i < lengSen + 2)
+            ||
+            i === lengPara - 1
         ) {
-            if (Math.random() < comma) {
+            if (Math.random() < period) {
+                newString += "。";
+            } else if (Math.random() < comma) {
                 newString += "，";
-            } else {
-                newString += "。";                
+            } else if (Math.random() < semicolon) {
+                newString += "；";
+            } else if (Math.random() < question) {
+                newString += "？";
+            } else if (Math.random() < exclamation) {
+                newString += "！";
             }
         }
     }
+
     if (
-        newString[newString.length - 1] === "，"
+        newString.slice(-1) === "，"
         ||
-        newString[newString.length - 1] === "。"
+        newString.slice(-1) === "；"
     ) {
         newString = newString.slice(0, -1);
+        newString += "。";
+    } else if (
+        !"。，；？！".includes(newString.slice(-1))
+    ) {
+        newString += "。？！"[parseInt(Math.random() * 3)];
     }
-    newString += "。";
 
     return newString;
 }
 
-function checkInput() {
-    leng    = parseInt(document.getElementById("length").value);
-    punc    = parseFloat(document.getElementById("punc").value);
-    comma   = parseFloat(document.getElementById("comma").value);
-    start   = parseInt(document.getElementById("start").value);
-    end     = parseInt(document.getElementById("end").value);
-    if (isNaN(leng)) leng = 1000;
-    if (isNaN(punc)) punc = 0.2;
-    if (isNaN(comma)) comma = 0.8;
-    if (isNaN(start)) start = 0;
-    if (isNaN(end)) end = 3000;
-}
-
 document.getElementById("generate").addEventListener("click", generate);
 document.getElementById("append").addEventListener("click", append);
-// document.querySelector("input").addEventListener("onHover", showAltText);
-
-// function showAltText() {
-//     leng = 1;
-// }
+document.getElementById("clear").addEventListener("click", clear);
 
 function generate() {
     checkInput();
-    content.textContent = createString(
-        leng,
-        punc,
-        comma,
-        start,
-        end
-    );
+    document.querySelector("p").textContent = createString();
 }
 
 function append() {
-    // console.log(createString(1000, 0.1, 0.8));
-    // content.textContent = createString(1000, 0.1, 0.8);
-    // let para = ;
     generate();
-    document.querySelector("body").appendChild(
+    document.getElementById("content2").appendChild(
         document.querySelector("p").cloneNode(true)
     );
+}
 
+function clear() {
+    let g = document.getElementById("content2");
+    while (g.hasChildNodes()) {
+        g.removeChild(g.firstChild);
+    }
 }
